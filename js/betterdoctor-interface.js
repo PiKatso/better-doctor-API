5772956cee2c38974b601dea8ab4d5aad305b0f3
Kat Gifford <katgiff2007@gmail.com>
2017-05-26 09:33:42 -0700

    add interface basic logic

--- js/betterdoctor-interface.js
+++ js/betterdoctor-interface.js
@@ -1,5 +1,9 @@
 var Doctor = require('./../js/betterdoctor.js').doctorModule;
 
 $(document).ready(function(){
-
+  var currentMedicalIssue = new Doctor();
+  $('#find-symptoms').click(function(){
+    var medicalIssue = $('#symptom').val();
+    currentMedicalIssue.getDoctor(medicalIssue);
+  });
 });
