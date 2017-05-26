49e0dc0feec6a8f6f45118e3385ed5bce1a3e837
Kat Gifford <katgiff2007@gmail.com>
2017-05-26 11:57:41 -0700

    refactor code

--- js/betterdoctor.js
+++ js/betterdoctor.js
@@ -7,13 +7,16 @@ function Doctor(medicalIssue){
 Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-    $('.showDoctors').text(result.data.forEach(function(data){
-      $('.showDoctors').append("<h5>Dr. " + data.profile.first_name + " " + data.profile.last_name + "</h5><p>" + data.specialties[0].actor + ", " + data.specialties[0].description + "</p><br>");
-     }));
+     (result.data.forEach(function(data){
+      var docName = data.profile.first_name + " " + data.profile.last_name;
+      var docSpeacialties = data.specialties[0].actor + ", " + data.specialties[0].description;
+
+     $('.showDoctors').append("<h5>Dr. " + docName + "</h5><p>" + docSpeacialties + "</p><br>");
+    }));
    })
 
    .fail(function(error){
-     $('.showDoctors').text("No doctors found that met your search criteria")
+     $('.showDoctors').text("No doctors found that met your search criteria");
     });
 };
 
