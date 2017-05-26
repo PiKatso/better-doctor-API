294ad571207981fad529b0b3f1db5cdf837d675b
Kat Gifford <katgiff2007@gmail.com>
2017-05-26 11:23:42 -0700

    add some styling, adjusting data

--- js/betterdoctor.js
+++ js/betterdoctor.js
@@ -8,7 +8,7 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
     $('.showDoctors').text(result.data.forEach(function(data){
-      $('.showDoctors').append("<p>Dr. " + data.profile.first_name + " " + data.profile.last_name + " " + data.profile.image_url + "</p>");
+      $('.showDoctors').append("<h5>Dr. " + data.profile.first_name + " " + data.profile.last_name + "</h5><p>" + data.specialties + "</p><br>");
      }));
    })
 
